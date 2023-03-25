7aa1ded953a2de4699de0fe47ba28b0c002ce3e6
Marçal <byelmarcal@hotmail.com>
2023-03-25 11:45:58 -0300

    first commit

---
+++ src/main/java/com/coursespring/springas/SpringasApplication.java
@@ -0,0 +1,13 @@
+package com.coursespring.springas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coursespring/springas/SpringasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coursespring.springas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
